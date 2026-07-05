b2b91a85735bc45e0b46ab2bbdecdff959f4b2ca
fu1se <fuisegamer1008@gmail.com>
2026-07-05 13:17:02 +0500

    chore: версия Android-приложения 0.7.1
    Синхронизирована с новым релизом (фикс отображения инвайт-токена
    mesh-сети в spur-gui).

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.spur.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.7.0"
+        versionCode = 4
+        versionName = "0.7.1"
     }
 
     buildTypes {
